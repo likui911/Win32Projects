32dff504f34cc2f8ebd4bcf1407683c1fd78b15a
likui911 <Likui_911@163.com>
2017-08-02 23:54:40 +0800

    Analog Clock

---
+++ AnalogClock/basewin.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+#include <Windows.h>
+
+template<typename DERIVED_TYPE>
+class BaseWindow
+{
+public:
+	BaseWindow() :m_hwnd{ NULL } {}
+
+	~BaseWindow() {}
+
+	static LRESULT  CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		DERIVED_TYPE* pThis = NULL;
+		if (uMsg == WM_NCCREATE)
+		{
+			CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+			pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
+			pThis->m_hwnd = hwnd;
+		}
+		else
+		{
+			pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+		}
+		if (pThis)
+		{
+			return pThis->HandleMessage(uMsg, wParam, lParam);
+		}
+		else
+		{
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+
+	BOOL Create(LPCTSTR lpWindowName, DWORD dwStyle = WS_OVERLAPPEDWINDOW,
+		int x = CW_USEDEFAULT, int y = CW_USEDEFAULT,
+		int nWidth = CW_USEDEFAULT, int nHeight = CW_USEDEFAULT,
+		HWND hWndParent = NULL, HMENU hMenu = NULL, HINSTANCE hInstance = 0);
+	
+	HWND Window()const { return m_hwnd; }
+
+protected:
+	virtual PCWSTR  ClassName()const = 0;
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+	HWND m_hwnd;
+};
+
+template<class DERIVED_TYPE>
+BOOL BaseWindow<DERIVED_TYPE>::Create( LPCTSTR lpWindowName, DWORD dwStyle,
+	int x, int y, int nWidth, int nHeight,
+	HWND hWndParent, HMENU hMenu, HINSTANCE hInstance)
+{
+	WNDCLASS wndclass;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hCursor = (HCURSOR)LoadImage(
+		NULL, IDC_ARROW, IMAGE_CURSOR, SM_CXCURSOR, SM_CYCURSOR, LR_VGACOLOR);
+	wndclass.hIcon = (HICON)LoadImage(
+		NULL, IDI_APPLICATION, IMAGE_ICON, SM_CXICON, SM_CYICON, LR_VGACOLOR);
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = DERIVED_TYPE::WindowProc;
+	wndclass.lpszClassName = ClassName();
+	wndclass.lpszMenuName = NULL;
+	wndclass.style = CS_VREDRAW | CS_HREDRAW;
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("WndClass register failed"), TEXT("Analog Clock"), MB_OK);
+		return FALSE;
+	}
+
+	m_hwnd = CreateWindow(ClassName(), lpWindowName, dwStyle, x, y,
+		nWidth, nHeight, hWndParent, hMenu, hInstance, this);
+	return (m_hwnd ? TRUE : FALSE);
+}
---
+++ AnalogClock/clock.cpp
@@ -0,0 +1,122 @@
+﻿#include <atlbase.h>
+#include <d2d1.h>
+#pragma comment(lib,"d2d1")
+#include "basewin.h"
+
+class AnalogClock :public BaseWindow<AnalogClock>
+{
+public:
+	AnalogClock() :pFactory{ NULL }, pRenderTarget{ NULL }, pColorBrush{ NULL } {}
+
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+	void OnPaint();
+	HRESULT CreateGraphicsResource();
+	void DrawDial();
+
+	PCWSTR ClassName() const { return TEXT("Analog Clock"); }
+private:
+	CComPtr<ID2D1Factory> pFactory;
+	CComPtr<ID2D1HwndRenderTarget> pRenderTarget;
+	CComPtr<ID2D1SolidColorBrush> pColorBrush;
+};
+
+LRESULT AnalogClock::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		if (FAILED(D2D1CreateFactory(
+			D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
+		{
+			return -1;  // Fail CreateWindowEx.
+		}
+		return 0;
+
+	case WM_SIZE:
+		InvalidateRect(m_hwnd, NULL, FALSE);
+		return 0;
+
+	case WM_PAINT:
+		OnPaint();
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	default:
+		return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+	}
+	return TRUE;
+}
+
+void AnalogClock::OnPaint()
+{
+	HRESULT hr = CreateGraphicsResource();
+	if (SUCCEEDED(hr))
+	{
+		PAINTSTRUCT ps;
+		BeginPaint(m_hwnd, &ps);
+
+		pRenderTarget->BeginDraw();
+		pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Orange, 1.0f));
+		
+		DrawDial();//表盘
+		//todo 绘制指针
+		
+		pRenderTarget->EndDraw();
+
+		EndPaint(m_hwnd, &ps);
+	}
+}
+
+HRESULT AnalogClock::CreateGraphicsResource()
+{
+	HRESULT hr = S_OK;
+	if (pRenderTarget == NULL)
+	{
+		RECT rect;
+		GetClientRect(m_hwnd, &rect);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rect.right, rect.bottom);
+
+		hr = pFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(m_hwnd, size), &pRenderTarget);
+
+		if (SUCCEEDED(hr))
+		{
+			D2D1_COLOR_F color = D2D1::ColorF(D2D1::ColorF::Orange,1.0f);
+			pRenderTarget->CreateSolidColorBrush(color, &pColorBrush);
+		}
+	}
+	return hr;
+}
+
+void AnalogClock::DrawDial()
+{
+	//todo 绘制表盘椭圆，刻度
+	pColorBrush->SetColor(D2D1::ColorF(D2D1::ColorF::LightSkyBlue, 1.0f));
+	pRenderTarget->FillEllipse(D2D1::Ellipse(D2D1::Point2F(100, 100), 50, 50),pColorBrush);
+
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	AnalogClock analog;
+	if (!analog.Create(TEXT("Analog Clock"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300))
+	{
+		return 0;
+	}
+
+	ShowWindow(analog.Window(), nCmdShow);
+
+	//run message handle
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+
