a28b448f3e18aad15f0341639125bceefc4e071f
likui911 <Likui_911@163.com>
2017-08-16 07:32:32 +0800

    icon menu accel

---
+++ menudemo/Resource.h
@@ -0,0 +1,55 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by menudemo.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MENUDEMO_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_CURSOR1                     106
+#define IDI_SMALL                       108
+#define IDC_MENUDEMO                    109
+#define IDR_MAINFRAME                   128
+#define IDC_POPMENU                     129
+#define IDR_MENU_COPY                   130
+#define IDR_MENU_CUT                    131
+#define IDR_MENU_PASTE                  132
+#define ID_FILE_NEW                     32771
+#define ID_FILE_OPEN                    32772
+#define ID_FILE_SAVE                    32773
+#define ID_FILE_SAVE_AS                 32774
+#define ID_FILE_IDM                     32775
+#define ID_EDIT_UNDO                    32777
+#define ID_EDIT_CUT                     32778
+#define ID_EDIT_COPY                    32779
+#define ID_EDIT_PASTE                   32780
+#define ID_EDIT_CLEAR                   32781
+#define ID_HELP_HELP                    32782
+#define IDM_HELP                        32783
+#define ID_MENU_COPY                    32784
+#define ID_MENU_CUT                     32785
+#define ID_MENU_PASTE                   32786
+#define IDM_BKGND_WHITE                 40011
+#define IDM_BKGND_LTGRAY                40012
+#define IDM_BKGND_GRAY                  40013
+#define IDM_BKGND_DKGRAY                40014
+#define IDM_BKGND_BLACK                 40015
+#define IDM_TIMER_START                 40016
+#define IDM_TIMER_STOP                  40017
+#define IDM_APP_HELP                    40018
+#define IDM_APP_ABOUT                   40019
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32791
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ menudemo/menudemo.cpp
@@ -0,0 +1,175 @@
+﻿// menudemo.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "menudemo.h"
+
+#define ID_TIMER 1
+TCHAR szAppName[] = TEXT("MenuDemo");
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK AboutWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	HWND hwnd;
+	HACCEL hAccel;
+	MSG msg;
+	WNDCLASS wndclass;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wndclass.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = MAKEINTRESOURCE(IDC_MENUDEMO);
+	wndclass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("This program requires Windows NT!"),
+			szAppName, MB_ICONERROR);
+		return 0;
+	}
+
+	hwnd = CreateWindow(szAppName, TEXT("Menu Demonstration"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MENUDEMO));
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(hwnd, hAccel, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static int idColor[5] = { WHITE_BRUSH, LTGRAY_BRUSH, GRAY_BRUSH,
+		DKGRAY_BRUSH, BLACK_BRUSH };
+	static int iSelection = IDM_BKGND_WHITE;
+	HMENU hMenu;
+	static HMENU hPopMenu;
+	POINT point;
+	switch (message)
+	{
+	case WM_CREATE:
+		hPopMenu = LoadMenu((HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), MAKEINTRESOURCE(IDC_POPMENU));
+		hPopMenu = GetSubMenu(hPopMenu, 0);
+		return 0;
+
+	case WM_RBUTTONDOWN:
+		point.x = LOWORD(lParam);
+		point.y = HIWORD(lParam);
+		ClientToScreen(hwnd, &point);
+		TrackPopupMenu(hPopMenu, TPM_RIGHTBUTTON, point.x, point.y, 0, hwnd, NULL);
+		return 0;
+
+	case WM_COMMAND:
+		hMenu = GetMenu(hwnd);
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_NEW:
+		case ID_FILE_OPEN:
+		case ID_FILE_SAVE:
+		case ID_FILE_SAVE_AS:
+			MessageBeep(0);
+			return 0;
+
+		case IDR_MENU_COPY:
+		case IDR_MENU_CUT:
+		case IDR_MENU_PASTE:
+			MessageBeep(0);
+			return 0;
+
+		case IDM_EXIT:
+			SendMessage(hwnd, WM_CLOSE, 0, 0);
+			return 0;
+
+		case ID_EDIT_UNDO:
+		case ID_EDIT_CUT:
+		case ID_EDIT_COPY:
+		case ID_EDIT_PASTE:
+		case ID_EDIT_CLEAR:
+			MessageBeep(0);
+			return 0;
+
+		case IDM_BKGND_WHITE:
+		case IDM_BKGND_LTGRAY:
+		case IDM_BKGND_GRAY:
+		case IDM_BKGND_DKGRAY:
+		case IDM_BKGND_BLACK:
+			CheckMenuItem(hMenu, iSelection, MF_UNCHECKED);
+			iSelection = LOWORD(wParam);//低位菜单ID,高位状态
+			CheckMenuItem(hMenu, iSelection, MF_CHECKED);
+			SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND,
+				(LONG)GetStockObject(idColor[LOWORD(wParam) - IDM_BKGND_WHITE]));
+			InvalidateRect(hwnd, NULL, TRUE);
+			return 0;
+
+		case IDM_TIMER_START:
+			if (SetTimer(hwnd, ID_TIMER, 1000, NULL))
+			{
+				EnableMenuItem(hMenu, IDM_TIMER_START, MF_GRAYED);
+				EnableMenuItem(hMenu, IDM_TIMER_STOP, MF_ENABLED);
+			}
+			return 0;
+
+		case IDM_TIMER_STOP:
+			KillTimer(hwnd, ID_TIMER);
+			EnableMenuItem(hMenu, IDM_TIMER_START, MF_ENABLED);
+			EnableMenuItem(hMenu, IDM_TIMER_STOP, MF_GRAYED);
+			return 0;
+
+		case IDM_HELP:
+			MessageBox(hwnd, TEXT("Help not yet implemented!"),
+				szAppName, MB_ICONEXCLAMATION | MB_OK);
+			return 0;
+
+		case IDM_ABOUT:
+			DialogBox((HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), 
+				MAKEINTRESOURCE(IDM_APP_ABOUT), hwnd, AboutWndProc);
+			return 0;
+		}
+		break;
+	case WM_TIMER:
+		MessageBeep(0);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+BOOL CALLBACK AboutWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			return TRUE;
+		}
+		break;
+	}
+	return FALSE;
+}
