f5a34d6a47742f0ad3b3ffafc99027bc506bb645
likui911 <Likui_911@163.com>
2017-08-21 22:32:50 +0800

    poppad

--- POPPAD/POPPAD.cpp
+++ POPPAD/POPPAD.cpp
@@ -1,15 +1,9 @@
 // POPPAD.cpp : Defines the entry point for the application.
 //
-
 #include "stdafx.h"
 #include "POPPAD.h"
 
 #define EDIT_ID 1
-
-
-static WCHAR szAppName[] = TEXT("POPPAD");
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int icmdShow)
 {
@@ -59,6 +53,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	static HWND hwndEdit;
 	static HINSTANCE hInst;
+	static BOOL bNeedSave = FALSE;
+	static TCHAR szFileName[MAX_PATH], szTitleName[MAX_PATH];
+
 	switch (msg)
 	{
 	case WM_CREATE:
@@ -68,6 +65,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			ES_LEFT | ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_NOHIDESEL, 0, 0, 0, 0,
 			hwnd, (HMENU)EDIT_ID, hInst, NULL);
 		SendMessage(hwndEdit, EM_LIMITTEXT, 32000, 0L);
+
+		PopFileInitialize(hwnd);
 		return 0;
 
 	case WM_SETFOCUS:
@@ -78,9 +77,108 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		MoveWindow(hwndEdit, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
 		return 0;
 
+	case WM_COMMAND:
+		if (lParam&&LOWORD(wParam) == EDIT_ID)
+		{
+			switch (HIWORD(wParam))
+			{
+			case EN_UPDATE:
+				bNeedSave = TRUE;
+				return 0;
+			}
+		}
+
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_NEW:
+			if (bNeedSave&&IDCANCEL == AskForSave(hwnd, szTitleName))
+			{
+				return 0;
+			}
+			SetWindowText(hwndEdit, TEXT("\0"));
+			szFileName[0] = '\0';
+			szTitleName[0] = '0';
+			bNeedSave = FALSE;
+			return 0;
+
+		case ID_FILE_OPEN:
+			if (bNeedSave&&IDCANCEL == AskForSave(hwnd, szTitleName))
+			{
+				return 0;
+			}
+			return 0;
+
+		case ID_FILE_SAVE:
+			
+			return 0;
+
+		case ID_FILE_SAVEAS:
+
+			return 0;
+
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hwnd, AboutWndProc);
+			return 0;
+		}
+		return 0;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
 	}
 	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+BOOL CALLBACK AboutWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			EndDialog(hwnd, TRUE);
+		}
+		break;
+	}
+	return FALSE;
+
+}
+
+int AskForSave(HWND hwnd, WCHAR* szTitleName)
+{
+
+	TCHAR szBuffer[64 + MAX_PATH];
+	int iReturn;
+	wsprintf(szBuffer, TEXT("Save current changes in %s?"), szTitleName[0] ? szTitleName : UNTITLED);
+	iReturn = MessageBox(hwnd, szBuffer, szAppName, MB_YESNOCANCEL | MB_ICONQUESTION);
+	if (iReturn == IDYES)
+	{
+		if (!SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0))
+			return IDCANCEL;
+	}
+	return iReturn;
+}
+
+void PopFileInitialize(HWND hwnd)
+{
+	TCHAR *szFileFilter   = TEXT("Text Files(*TXT)\0*.txt\0All Files(*.*)\0*.*\0\0");
+	ofn.lStructSize       = sizeof(OPENFILENAME);
+	ofn.hwndOwner         = hwnd;
+	ofn.hInstance         = NULL;
+	ofn.lpstrFilter       = szFileFilter;
+	ofn.lpstrCustomFilter = NULL;
+	ofn.nMaxCustFilter    = NULL;
+	ofn.nFilterIndex      = 0;
+	ofn.lpstrFile         = NULL;
+	ofn.nMaxFile          = MAX_PATH;
+	ofn.lpstrFileTitle    = NULL;
+	ofn.lpstrInitialDir   = NULL;
+	ofn.lpstrTitle        = NULL;
+	ofn.Flags             = 0;
+	ofn.nFileOffset       = 0;
+	ofn.nFileExtension    = 0;
+	ofn.lpstrDefExt       = TEXT("txt");
+	ofn.lCustData         = 0L;
+	ofn.lpfnHook          = NULL;
+	ofn.lpTemplateName    = NULL;
 }
--- POPPAD/POPPAD.h
+++ POPPAD/POPPAD.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "resource.h"
+#include <commdlg.h>
+
+#define UNTITLED TEXT("UNTITLED")
+
+static WCHAR szAppName[] = TEXT("POPPAD");
+int AskForSave(HWND hwnd, WCHAR* szTitleName);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK AboutWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+static OPENFILENAME ofn;
+void PopFileInitialize(HWND hwnd);
+
+
